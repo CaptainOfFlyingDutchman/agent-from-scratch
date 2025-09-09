2b754b78b96cd25842c4187125351c110527a581
Manvendra Singh <manvendrask2@gmail.com>
2025-09-10 01:46:06 +0530

    feat: work with vercel ai sdk

--- src/ai.ts
+++ src/ai.ts
@@ -1,3 +1,19 @@
-import OpenAI from 'openai'
+import { createOpenRouter } from '@openrouter/ai-sdk-provider'
+import { generateText } from 'ai'
+import dotenv from 'dotenv'
 
-export const openai = new OpenAI()
+dotenv.config()
+
+const openRouter = createOpenRouter({
+  apiKey: process.env.AI_KEY,
+})
+
+export async function runLLM(userMessage: string) {
+  const { text } = await generateText({
+    model: openRouter.chat('openai/gpt-4o-mini'),
+    temperature: 0.1,
+    messages: [{ role: 'user', content: userMessage }],
+  })
+
+  return text
+}
