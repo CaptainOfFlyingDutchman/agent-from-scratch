17bcab882fed50b95cac347ae0c404355c110e4b
Manvendra Singh <manvendrask2@gmail.com>
2025-09-10 02:23:01 +0530

    feat: use openai package with openrouter

--- src/llm.ts
+++ src/llm.ts
@@ -1,11 +1,20 @@
-import { openai } from './ai'
+import { OpenAI } from 'openai'
+
+const openai = new OpenAI({
+  apiKey: process.env.OPEN_AI_KEY,
+  baseURL: 'https://openrouter.ai/api/v1',
+  defaultHeaders: {
+    'Http-Referer': 'https://manvendrask.com',
+    'X-Title': 'Learning AI'
+  }
+})
 
 export const runLLM = async ({ userMessage }: { userMessage: string }) => {
-  const response = await openai.chat.completions.create({
-    model: 'gpt-4o-mini',
+  const { choices } = await openai.chat.completions.create({
+    model: 'openai/gpt-4o-mini',
     temperature: 0.1,
-    messages: [{ role: 'user', content: userMessage }],
+    messages: [{ role: 'user', content: userMessage }]
   })
 
-  return response.choices[0].message.content
+  return choices[0].message.content
 }
